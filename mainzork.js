1c75c960ef9ef04947fe4d0fa135f94d872ca8d6
Habenzy <robertrstauss@gmail.com>
2018-09-26 15:19:20 -0500

    rooms blocked in

--- mainzork.js
+++ mainzork.js
@@ -13,7 +13,40 @@ function moveToRoom(newRoom) {
     currentRoom = newRoom
   }
 }
+state = {
+  "mainSt": {canChangeTo: ["foyer", "mrMikes", "muddyWaters"]},
+  "mrMikes": {canChangeTo: ["mainSt"]},
+  "muddyWaters": {canChangeTo: ["mainSt"]},
+  "foyer": {canChangeTo: ["mainSt", "classRoom"]},
+  "classRoom": {canChangeTo: ["foyer"]}
 
-let mainStreet = {
+};
+
+
+const mainSt = {
+  mainDoor : 'isLocked',
+  sign : 'Welcome to Burlington Code Academy!/n Come on up to the second floor./n If the door is locked, use the code12345.'
   
-}
+};
+
+const mrMikes = {
+
+};
+
+const muddyWaters = {
+
+};
+
+const foyer = {
+
+};
+
+const classRoom = {
+
+};
+
+/*class Room {
+  constructor() {
+    this.state = {}
+  }
+}*/
