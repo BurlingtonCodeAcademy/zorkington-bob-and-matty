3b494c0ee2f40b702f8762e5d3f5a142ee205211
Habenzy <robertrstauss@gmail.com>
2018-09-27 12:05:17 -0500

    room movement deleted

--- mainzork.js
+++ mainzork.js
@@ -64,19 +64,6 @@ state = {
 
 let currentRoom = mainSt;
 
-let validTransition = currentRoom.canChangeTo;
-if (validTransition.includes(nextRoom)) {
-  currentRoom = nextRoom;
-}
-else {
-  throw 'invalid path from ' + currentRoom + ' to ' + nextRoom;
-};
-
-function moveToRoom(newRoom) {
-  if (validTransition.includes(newRoom)) {
-    currentRoom = newRoom
-  }
-}//Room[currentRoom] = undefined line 67 changed to currentRoom from Room[currentRoom]
 
 
 console.log(currentRoom);
