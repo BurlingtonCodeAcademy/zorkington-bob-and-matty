683e1d9788995f5c58d8d5c1e38e1a71d244125b
Habenzy <robertrstauss@gmail.com>
2018-09-27 12:07:28 -0500

    added room change state back in, still non functional

--- mainzork.js
+++ mainzork.js
@@ -64,7 +64,21 @@ state = {
 
 let currentRoom = mainSt;
 
-
+function enterRoom (nextRoom) {
+  let validTransition = room[currentRoom].canChangeTo;
+  if (validTransition.includes(nextRoom)) {
+    currentRoom = nextRoom;
+  }
+  else {
+    throw 'invalid path from ' + currentRoom + ' to ' + nextRoom;
+  }
+}
+let currentRoom = null;
+function moveToRoom(newRoom) {
+  if (canMoveToRoom(newRoom)) {
+    currentRoom = newRoom
+  }
+}
 
 console.log(currentRoom);
 moveToRoom(mrMikes);
